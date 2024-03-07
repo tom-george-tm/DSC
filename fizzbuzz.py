a0e9e44cdae7f89d61d23f266b4e733a4bf522c7
tom-george-tm <tom.george@thoughtminds.io>
2024-03-07 17:56:49 +0530

    check the condition for 5 to

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,11 @@
 def fizzbuzz(num):
    if num % 2 == 0 and num % 5 == 0 :
       return "fizzbuzz"
-   elif( num % 2 == 0):
+   elif num % 2 == 0:
       return "fizz"
-   else:
+   elif num % 5==0:
       return "buzz" 
+   else :
+      return num 
    
 print(fizzbuzz(100))   
